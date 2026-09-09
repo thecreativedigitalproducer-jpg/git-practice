a998c45d2bd4fb7c606e88b7498abdc69b5053ab
The Creative Digital Producer <thecreativedigitalproducer@gmail.com>
2026-09-09 11:22:38 +0200

    Add about route

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,3 +5,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return "Welcome to KRN Dev Engine"
+
+@main.route("/about")
+def about():
+    return "This is the KRN Dev Engine."
