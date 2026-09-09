e22d0bf98a96f3cae6df2dcd32547455f9961ccb
The Creative Digital Producer <thecreativedigitalproducer@gmail.com>
2026-09-09 20:11:43 +0200

    Add auth blueprint

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app.routes.main import main
 from app.routes.users import users
+from app.routes.auth import auth
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main)
     app.register_blueprint(users)
+    app.register_blueprint(auth)
     return app
